3b3eaa2bbc4fd1a07c29801abf49ba2495a062af
Ondřej Hošek <ondra.hosek@gmail.com>
2014-06-07 18:45:35 +0200

    remove superfluous references and using directives

--- DistributePrintJobs/HttpListener.cs
+++ DistributePrintJobs/HttpListener.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using DotLiquid;
 using Newtonsoft.Json;
 
--- DistributePrintJobs/Program.cs
+++ DistributePrintJobs/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
